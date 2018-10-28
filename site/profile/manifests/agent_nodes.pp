d2e40729b4b6ada3cb2d9be7d20f0a2816533cd1
mateki <you@example.com>
2018-10-28 22:45:39 +0100

    imports

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -1,6 +1,6 @@
 class profile::agent_nodes {
   include dockeragent
-  include ssh
+  include ::ssh
   dockeragent::node { 'web.puppet.vm': }
   dockeragent::node { 'db.puppet.vm': }
   
@@ -8,6 +8,6 @@ ssh::keys:
   root_for_userX:
     ensure: present
     user: root
-    type: dsa
-    key: AAAAB3NzaC1yc2EAAAADAQABAAABAQCY0/hqI6DWf25N2Gr/oYiPmugyAZBWLQi4dBF91IdZtZWFfXBaJ1ZebM4t3JXABOQinWchEHjGpCI7y9jIRElYwm2L4C1DiYY9lET5R39YNO1hJfXhymwXqZgDeWiXzpStG3Oih1k9nDaIZe9A6uib73kJc1Ew8H8xutPi0r6STOxXv72El7C1ihQdYvIXvQH6FThHbiIP+sJbjvjvdgCGVzdv4wYaFXyP8VfgqHxuTCv3us/2/onecq2sK+jQW/ObH9y4duFDRrPsBoEzwmdT8v17fpiQsERk9cF1DCLFPdb9ATCgmztHr8DSKh5lcHYDe0wwLS/YgLF+mComAZ/f
+    type: rsa
+    key: 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCY0/hqI6DWf25N2Gr/oYiPmugyAZBWLQi4dBF91IdZtZWFfXBaJ1ZebM4t3JXABOQinWchEHjGpCI7y9jIRElYwm2L4C1DiYY9lET5R39YNO1hJfXhymwXqZgDeWiXzpStG3Oih1k9nDaIZe9A6uib73kJc1Ew8H8xutPi0r6STOxXv72El7C1ihQdYvIXvQH6FThHbiIP+sJbjvjvdgCGVzdv4wYaFXyP8VfgqHxuTCv3us/2/onecq2sK+jQW/ObH9y4duFDRrPsBoEzwmdT8v17fpiQsERk9cF1DCLFPdb9ATCgmztHr8DSKh5lcHYDe0wwLS/YgLF+mComAZ/f'
 }
