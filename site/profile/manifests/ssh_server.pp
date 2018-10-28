fb0cfb86e1244f408b949e50e300a6f4cc4796b8
mateki <you@example.com>
2018-10-28 23:11:36 +0100

    remove whie space

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCY0/hqI6DWf25N2Gr/oYiPmugyAZBWLQi4dBF91IdZtZWFfXBaJ1ZebM4t3JXABOQinWchEHjGpCI7y9jIRElYwm2L4C1DiYY9lET5R39YNO1hJfXhymwXqZgDeWiXzpStG3Oih1k9nDaIZe9A6uib73kJc1Ew8H8xutPi0r6STOxXv72El7C1ihQdYvIXvQH6FThHbiIP+sJbjvjvdgCGVzdv4wYaFXyP8VfgqHxuTCv3us/2/onecq2sK+jQW/ObH9y4duFDRrPsBoEzwmdT8v17fpiQsERk9cF1DCLFPdb9ATCgmztHr8DSKh5lcHYDe0wwLS/YgLF+mComAZ/f',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCY0/hqI6DWf25N2Gr/oYiPmugyAZBWLQi4dBF91IdZtZWFfXBaJ1ZebM4t3JXABOQinWchEHjGpCI7y9jIRElYwm2L4C1DiYY9lET5R39YNO1hJfXhymwXqZgDeWiXzpStG3Oih1k9nDaIZe9A6uib73kJc1Ew8H8xutPi0r6STOxXv72El7C1ihQdYvIXvQH6FThHbiIP+sJbjvjvdgCGVzdv4wYaFXyP8VfgqHxuTCv3us/2/onecq2sK+jQW/ObH9y4duFDRrPsBoEzwmdT8v17fpiQsERk9cF1DCLFPdb9ATCgmztHr8DSKh5lcHYDe0wwLS/YgLF+mComAZ/f',
 	}  
 }
