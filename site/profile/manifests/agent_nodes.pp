9339f6d6753b044049829932c6cd12c607a09798
mateki <mateki@o2.pl>
2018-11-02 17:33:25 +0100

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -2,6 +2,8 @@ class profile::agent_nodes {
   include dockeragent
   dockeragent::node { 'web.puppet.vm': }
   dockeragent::node { 'db.puppet.vm': }
+  dockeragent::node { 'minetest.puppet.vm': }
+
   host { 'web.puppet.vm': 
     ensure => present,
     ip => '192.168.0.18',
